7ec6ef26e8949bbd1dbbc2b586f080011d72e38d
shirangabriel <gabriel.stcb@gmail.com>
2024-11-11 20:32:40 +0530

    code clean up

--- app/Auth/index.tsx
+++ app/Auth/index.tsx
@@ -1,40 +1,11 @@
 import { ThemedText } from "@/components/ThemedText"
 import Screen from "../components/Screen"
-import { Button, Text, View } from "react-native"
+import { Button } from "react-native"
 import { Account } from "react-native-appwrite"
 import client from "@/config/appwriteClient"
-import { useEffect, useState } from "react"
-import auth from "@react-native-firebase/auth"
+
 
 const Auth = () => {
-     // Set an initializing state whilst Firebase connects
-    const [initializing, setInitializing] = useState(true);
-    const [user, setUser] = useState();
-
-    // Handle user state changes
-    function onAuthStateChanged(user) {
-        setUser(user);
-        if (initializing) setInitializing(false);
-    }
-
-    useEffect(() => {
-        const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
-        return subscriber; // unsubscribe on unmount
-    }, []);
-
-
-    if (initializing) return null;
-
-    // if (!user) {
-    //     return (
-    //     <View>
-    //         <Text>Login</Text>
-    //     </View>
-    //     );
-    // }
-    
-
-
     const handleLogin = () => {
         const account = new Account(client)
         const promise = account.createEmailPasswordSession('john.doe@widgetcorps.com', 'admin123')
